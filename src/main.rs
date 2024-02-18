84b9ce2aca5fbb4dfcc5cd4b279bbb19ac031dd5
Dai Nguyen <ngtrdai290197@gmail.com>
2024-02-18 12:56:27 +0700

    #1: end of the part 3

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+mod constants;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    // guess_secret_number();
+    // let x = {
+    //     let y = 1;
+    //     y+2
+    // };
+    //
+    // println!("The value of x is: {x}");
+
+    let mut name = "Dai Nguyen";
+    let copied = name;
+    println!("name = {}, copied = {}", name, copied);
+    name = "Dai Nguyen updated.";
+    println!("name = {}, copied = {}", name, copied);
+}
+
+fn guess_secret_number() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("Your guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
